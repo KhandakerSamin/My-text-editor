bb54422dc81106d8505fd033cd6d6c5c30b055a2
Khandaker Samin Yeasar <khandakersaminyeasar@gmail.com>
2023-08-16 19:50:04 +0600

    all done

--- script.js
+++ script.js
@@ -1,36 +1,52 @@
+// bold,italic,underline
+
 document.getElementById('btn-bold').addEventListener('click',function(){
     const textArea = document.getElementById('text-input');
-    textArea.setAttribute('class','font-bold');
+    textArea.style.fontWeight= 'bold';
 })
 document.getElementById('btn-italic').addEventListener('click',function(){
     const textArea = document.getElementById('text-input');
-    textArea.setAttribute('class','italic');
+    textArea.style.fontStyle='italic';
 })
 document.getElementById('btn-underline').addEventListener('click',function(){
     const textArea = document.getElementById('text-input');
-    textArea.setAttribute('class','underline');
+    textArea.style.textDecoration = "underline";
 })
+
+// Text Align :
+
 document.getElementById('btn-left').addEventListener('click',function(){
     const textArea = document.getElementById('text-input');
-    textArea.setAttribute('class','text-left');
+    textArea.style.textAlign='left';
 })
 document.getElementById('btn-right').addEventListener('click',function(){
     const textArea = document.getElementById('text-input');
-    textArea.setAttribute('class','text-right');
+    textArea.style.textAlign='right';
 })
 document.getElementById('btn-center').addEventListener('click',function(){
     const textArea = document.getElementById('text-input');
-    textArea.setAttribute('class','text-center');
+    textArea.style.textAlign='center';
 })
 document.getElementById('btn-justify').addEventListener('click',function(){
     const textArea = document.getElementById('text-input');
-    textArea.setAttribute('class','text-justify');
+    textArea.style.textAlign='justify';
 })
+
+
+// font size change
 
 document.getElementById('font-input').addEventListener('input',function(){
     const textArea = document.getElementById('text-input');
     const fontInput = document.getElementById('font-input');
     const fontInputValue = fontInput.value ;
-    console.log(fontInputValue)
-    // textArea.setAttribute('class',text(fontInputValue));
+    textArea.style.fontSize=fontInputValue+'px';
+})
+
+// font color change
+
+document.getElementById('color-input').addEventListener('input',function(){
+    const textArea = document.getElementById('text-input');
+    const fontInput = document.getElementById('color-input');
+    const colorInputValue = fontInput.value ;
+    textArea.style.color=colorInputValue;
 })
